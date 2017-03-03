08474cbc8a211c151a52ef05d8983e7f1945d2ef
Sean Matson <sean.r.matson@gmail.com>
2017-03-03 00:41:53 -0800

    started the option to do dynamic screen name lookup

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const express = require('express');
 const http = require('http');
 const twitter = require('twitter');
 const config = require('./config');
+const pg = require('pg');
 
 // app instance, port
-// let app = express();
-// let port = process.env.port || 8080;
+let app = express();
+let port = process.env.port || 8080;
 
 // no cache validation plz
 // app.disable('etag');
 
 let client = new twitter(config.twitter);
 
-let stream = client.stream('statuses/filter', {track: 'maga'}, function(stream) {
-  stream.on('data', function(event) {
-    console.log(event && event.text);
-  });
+ // todo make this dynamic, either cli or form submit
+ // we could also archive ids locally to reduce API calls
 
-  stream.on('error', function(error) {
-    throw error;
+client.get('users/lookup', {screen_name: "salted_hashtag"}, function(error, response) {
+  if(error) throw error;
+  console.log(response, response[0].id_str) // we should do error checking
+  let stream = client.stream('statuses/filter', {follow: response[0].id_str}, function(stream) {
+    stream.on('data', function(event) {
+      console.log(event && event.text);
+    });
+
+    stream.on('error', function(error) {
+      throw error;
+    });
   });
 });
-
-//let params = {screen_name: 'salted_hash'};
